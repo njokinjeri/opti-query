295d873c9f84bbc95ec30be3dcc2b6fbd3708627
njoki <njerinjoki22@gmail.com>
2026-02-04 17:07:12 +0300

    map out core sections & layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import Layout from "../src/components/layout/Layout"
+import Hero from "./components/sections/Hero"
+import Partners from "./components/sections/Partners"
+import Features from "./components/sections/Features"
+import Reviews from "./components/sections/Reviews"
+import CTA from "./components/sections/CTA"
+import Pricing from "./components/sections/Pricing"
+
 export default function App () {
-  
+    return (
+        <Layout>
+            <Hero />
+            <Partners />
+            <Features />
+            <Reviews />
+            <Pricing />
+            <CTA />
+        </Layout>
+    )
 }
+
